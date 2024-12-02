6a5fa129aaba72cc4e53bf102bbd75a2e922719e
Qweellow <iceqlips@gmail.com>
2024-12-02 20:24:18 +0500

    Python Junior module 1 lesson 6

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+work_completed=12
+hours_spent=1.5
+course_name='Python'
+time_for_one_task=0.125
+print(f'Курс: {course_name}',f'Всего задач: {work_completed}',f'Затрачено времени: {hours_spent}',f'Среднее время выполнения: {time_for_one_task}', sep= '\n')
